1132913b084a9a01feac2c26c59155f1a33c64ed
Hasan Chowdhury <chowdhury.hms@gmail.com>
2024-03-20 02:08:25 +0600

    Remove if-exists condition checking for the fallback functions

--- inc/base/others/fallback-already.php
+++ inc/base/others/fallback-already.php
@@ -1,16 +1,14 @@
 <?php
 defined('ABSPATH') or die('No direct access allowed!'); // Avoid direct file request
 
-if (!function_exists('daprods_skip_already_admin_notice')) {
-    /**
-     * Show an admin notice to administrators when the plugin is already active.
-     */
-    function daprods_skip_already_admin_notice() {
-        if (current_user_can('activate_plugins')) {
-            echo '<div class=\'notice notice-error\'>
-				<p>Currently multiple versions of the plugin <strong>Delete Wordpress Products</strong> are active. Please deactivate all versions except the one you want to use.</p>' .
-                '</div>';
-        }
+/**
+ * Show an admin notice to administrators when the plugin is already active.
+ */
+function daprods_skip_already_admin_notice() {
+    if (current_user_can('activate_plugins')) {
+        echo '<div class=\'notice notice-error\'>
+            <p>Currently multiple versions of the plugin <strong>Delete Wordpress Products</strong> are active. Please deactivate all versions except the one you want to use.</p>' .
+            '</div>';
     }
 }
 add_action('admin_notices', 'daprods_skip_already_admin_notice');
--- inc/base/others/fallback-wp-version.php
+++ inc/base/others/fallback-wp-version.php
@@ -1,28 +1,26 @@
 <?php
 defined('ABSPATH') or die('No direct access allowed!'); // Avoid direct file request
 
-if (!function_exists('daprods_skip_wp_admin_notice')) {
-    /**
-     * Show an admin notice to administrators when the minimum WP version
-     * could not be reached. The error message is only in english available.
-     */
-    function daprods_skip_wp_admin_notice() {
-        if (current_user_can('activate_plugins')) {
-            $data = get_plugin_data(DAPRODS_FILE, true, false);
-            global $wp_version;
-            echo '<div class=\'notice notice-error\'>
-				<p><strong>' .
-                esc_html($data['Name']) .
-                '</strong> could not be initialized because you need minimum WordPress version ' .
-                esc_html(DAPRODS_MIN_WP) .
-                ' ... you are running: ' .
-                esc_html($wp_version) .
-                '.
-				<a href="' .
-                esc_html(admin_url('update-core.php')) .
-                '">Update WordPress now.</a>
-			</div>';
-        }
+/**
+ * Show an admin notice to administrators when the minimum WP version
+ * could not be reached. The error message is only in english available.
+ */
+function daprods_skip_wp_admin_notice() {
+    if (current_user_can('activate_plugins')) {
+        $data = get_plugin_data(DAPRODS_FILE, true, false);
+        global $wp_version;
+        echo '<div class=\'notice notice-error\'>
+            <p><strong>' .
+            esc_html($data['Name']) .
+            '</strong> could not be initialized because you need minimum WordPress version ' .
+            esc_html(DAPRODS_MIN_WP) .
+            ' ... you are running: ' .
+            esc_html($wp_version) .
+            '.
+            <a href="' .
+            esc_html(admin_url('update-core.php')) .
+            '">Update WordPress now.</a>
+        </div>';
     }
 }
 add_action('admin_notices', 'daprods_skip_wp_admin_notice');

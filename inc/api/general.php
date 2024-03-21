ada13b90e092b6edddb9245e87bef266f1559141
Hasan Chowdhury <chowdhury.hms@gmail.com>
2024-03-21 15:04:33 +0600

    Remove feuction_exists from the general file

--- inc/api/general.php
+++ inc/api/general.php
@@ -1,24 +1,22 @@
 <?php
 
-if (!\function_exists('daprods_is_plugin_active')) {
+/**
+ * Checks if DAPRODS is active for the current user.
+ *
+ * @return boolean
+ * @since 3.1.0
+ */
+function daprods_is_plugin_active()
+{
     /**
-     * Checks if DAPRODS is active for the current user.
+     * Checks if DAPRODS is active for the current user. Do not use this filter
+     * yourself, instead use daprods_is_plugin_active() function!
      *
-     * @return boolean
-     * @since 3.1.0
+     * @param {boolean} True for activated and false for deactivated
+     * @return {boolean}
+     * @hook DAPRODS/Active
+     * @since 1.0.0
      */
-    function daprods_is_plugin_active()
-    {
-        /**
-         * Checks if DAPRODS is active for the current user. Do not use this filter
-         * yourself, instead use daprods_is_plugin_active() function!
-         *
-         * @param {boolean} True for activated and false for deactivated
-         * @return {boolean}
-         * @hook DAPRODS/Active
-         * @since 1.0.0
-         */
-        $result = \apply_filters('DAPRODS/Active', \current_user_can('manage_categories'));
-        return $result;
-    }
+    $result = \apply_filters('DAPRODS/Active', \current_user_can('manage_categories'));
+    return $result;
 }

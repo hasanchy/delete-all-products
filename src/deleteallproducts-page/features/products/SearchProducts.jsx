237f401f9f921ffcf9d3a8e336e666e24b2aeef1
Hasan Chowdhury <chowdhury.hms@gmail.com>
2025-03-29 07:11:59 +0600

    Add information about Product Cleaner for WooCommerce.

--- src/deleteallproducts-page/features/products/SearchProducts.jsx
+++ src/deleteallproducts-page/features/products/SearchProducts.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { Alert, Button, Card, Checkbox, Col, Form, Image, Row, Space } from "antd";
+import { Alert, Button, Card, Checkbox, Col, Form, Image, Row, Space, Typography } from "antd";
+import { CheckCircleOutlined } from '@ant-design/icons';
 import { __ } from "@wordpress/i18n";
 import { useDispatch, useSelector } from 'react-redux';
 import { searchProducts } from '../../services/apiService';
@@ -74,27 +75,39 @@ const SearchProducts = () => {
                     description={<>{__( 'Product deletion with filters is available in the', 'delete-all-products' )} <b><a href='https://woocommerce.com/products/product-cleaner-for-woocommerce/' target='_blank'>{__( 'Pro version', 'delete-all-products' )}</a></b> {__( 'of the plugin.', 'delete-all-products' )}</>}
                     type="info"
                 />
-                <Row>
+                <Row gutter={20}>
                     <Col span={16} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                         <a href='https://woocommerce.com/products/product-cleaner-for-woocommerce/' target='_blank'>
                             <Image
                                 preview={false}
                                 src={window.daprodsDeleteAllProducts.filtersImageSrc} 
-                                alt="Product Cleaner" style={{ width: '100%', border: '1px solid #000', borderRadius: '10px' }}
+                                alt="Product Cleaner" style={{ width: '100%', border: '1px solid #ccc', borderRadius: '10px', boxShadow: '0 1px 2px -2px rgba(0, 0, 0, 0.16),0 3px 6px 0 rgba(0, 0, 0, 0.12),0 5px 12px 4px rgba(0, 0, 0, 0.09)' }}
                                 
                             /> 
                         </a>
                     </Col>
-                    <Col span={8} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                        <Button
-                            key="link"
-                            href="https://woocommerce.com/products/product-cleaner-for-woocommerce/"
-                            type="primary"
-                            style={{ marginRight: '10px' }}
-                            target='_blank'
-                        >
-                            { __( 'Upgrade Now' ) }
-                        </Button>
+                    <Col span={8} style={{ display: 'flex'}}>
+                        <Space size={15} direction="vertical" style={{ width: '100%' }}>
+                            <div><Typography.Title level={4} style={{marginTop: 0}}>{__( 'Upgrade to Product Cleaner for WooCommerce', 'delete-all-products' )}</Typography.Title></div>
+                            <div>{__( 'With Product Cleaner for WooCommerce, you can effortlessly filter, delete, or trash WooCommerce products in bulk while tracking progress in real time for a seamless cleanup experience.', 'delete-all-products' )}</div>
+                            <div><CheckCircleOutlined style={{ color: '#52C41A', fontSize: '18px', marginRight: '8px' }} /> <Typography.Text strong>{__( 'Delete products with or without filters', 'delete-all-products' )}</Typography.Text></div>
+                            <div><CheckCircleOutlined style={{ color: '#52C41A', fontSize: '18px', marginRight: '8px' }} /> <Typography.Text strong>{__( 'Permanently delete products & images', 'delete-all-products' )}</Typography.Text></div>
+                            <div><CheckCircleOutlined style={{ color: '#52C41A', fontSize: '18px', marginRight: '8px' }} /> <Typography.Text strong>{__( 'Preview before deletion', 'delete-all-products' )}</Typography.Text></div>
+                            <div><CheckCircleOutlined style={{ color: '#52C41A', fontSize: '18px', marginRight: '8px' }} /> <Typography.Text strong>{__( 'Track progress in real-time', 'delete-all-products' )}</Typography.Text></div>
+                            <div><CheckCircleOutlined style={{ color: '#52C41A', fontSize: '18px', marginRight: '8px' }} /> <Typography.Text strong>{__( 'Get all future updates & improvements', 'delete-all-products' )}</Typography.Text></div>
+                            <div><CheckCircleOutlined style={{ color: '#52C41A', fontSize: '18px', marginRight: '8px' }} /> <Typography.Text strong>{__( 'Enjoy priority support', 'delete-all-products' )}</Typography.Text></div>
+                            <div style={{ width: '100%', textAlign: 'center' }}>
+                                <Button
+                                    key="link"
+                                    href="https://woocommerce.com/products/product-cleaner-for-woocommerce/"
+                                    type="primary"
+                                    style={{ marginTop: '20px' }}
+                                    target='_blank'
+                                >
+                                    { __( 'Upgrade Now' ) }
+                                </Button>
+                            </div>
+                        </Space>
                     </Col>
                 </Row>
             </Space>
--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,6 +6,4 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
-    'PHPCSStandards\\Composer\\Plugin\\Installers\\PHPCodeSniffer\\' => array($vendorDir . '/dealerdirect/phpcodesniffer-composer-installer/src'),
-    'CoenJacobs\\Mozart\\' => array($vendorDir . '/coenjacobs/mozart/src'),
 );
